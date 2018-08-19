050d9652892cb923203d453e087d570bd63feb9f
DeRidder <p.b.deridder@hotmail.com>
2018-08-19 17:15:20 +0200

    use seperate class per exercise

--- ruby-project/exercise_1_test.rb
+++ ruby-project/exercise_1_test.rb
@@ -1,19 +1,19 @@
-require_relative 'universe'
+require_relative 'exercise_1'
 
 def success(success)
   puts "TECHIO> success #{success}"
 end
 
-def msg(channel, msg)
+def msg(msg)
   puts "TECHIO> message \"#{msg}\""
 end
 
 def existsInFile(str)
-  return File.foreach("conditional_exercise.rb").any?{ |l| l[str] }
+  return File.foreach("exercise_1.rb").any?{ |l| l[str] }
 end
 
 if existsInFile('if')
-  if ConditionalExercise.if_exercise == 'Apple iPhone X'
+  if exercise_1 == 'Apple iPhone X'
     success(true)
   else
     success(false)

a1ad82a2d98d388ba897fe98208b3041fcd2a04d
DeRidder <p.b.deridder@hotmail.com>
2018-08-20 20:34:39 +0200

    add conditional exercise

---
+++ ruby-project/conditionals/exercise_4.rb
@@ -0,0 +1,8 @@
+def exercise_4  
+  iphone_x_stock = 0  
+
+  # 1. Gebruik alleen een unless-statement 
+  # 2. Toon de String "Helaas, de Apple iPhone X is tijdelijk uitverkocht" als de 
+  #    iphone_x_stock niet op voorraad is 
+
+end
